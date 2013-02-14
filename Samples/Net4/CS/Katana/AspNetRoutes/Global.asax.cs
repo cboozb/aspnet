98ed608405ff2d16cdc1bf409d51bcae08ed2ea1
Tratcher <Tratcher@hotmail.com>
2013-02-14 09:55:51 -0800

    Import Katana samples.

---
+++ Samples/Net4/CS/Katana/AspNetRoutes/Global.asax.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Owin.Host.SystemWeb;
+using Owin;
+using System;
+using System.Web.Routing;
+
+namespace AspNetRoutes
+{
+    public class Global : System.Web.HttpApplication
+    {
+        // How to hook OWIN pipelines into the normal Asp.Net route table side by side with other components.
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            // TODO: In recent builds MapOwinRoute now uses AspNet route pattern matching.
+            // MapOwinPath should be used for the basic path match below.
+
+            // Paths that start with /owin will be directed to our startup class.
+            RouteTable.Routes.MapOwinRoute("/owin");
+
+            RouteTable.Routes.MapOwinRoute("/special", builder =>
+            {
+                builder.UseHandler(new StartupExtensions.OwinHandlerAsync(OwinApp2.Invoke));
+            });
+        }
+    }
+}
---
+++ Samples/Net4/CS/Katana/Embedded/Startup.cs
@@ -0,0 +1,34 @@
+﻿using Owin;
+using Owin.Types;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Embedded
+{
+    public class Startup
+    {
+        // Invoked once at startup to configure your application.
+        public void Configuration(IAppBuilder builder)
+        {
+            builder.UseHandler(new StartupExtensions.OwinHandlerAsync(Invoke));
+        }
+
+        // Invoked once per request.
+        public Task Invoke(OwinRequest request, OwinResponse response)
+        {
+            string responseText = "Hello World";
+            byte[] responseBytes = Encoding.UTF8.GetBytes(responseText);
+
+            // TODO: Should Content-Length be a strong property?
+            response.SetHeader("Content-Length", responseBytes.Length.ToString(CultureInfo.InvariantCulture));
+            response.SetHeader("Content-Type", "text/plain");
+
+            // TODO: OwinResponse needs a WriteAsync method.
+            Stream responseStream = response.Body;
+            return Task.Factory.FromAsync(responseStream.BeginWrite, responseStream.EndWrite, responseBytes, 0, responseBytes.Length, null);
+            // 4.5: return responseStream.WriteAsync(responseBytes, 0, responseBytes.Length);
+        }
+    }
+}
