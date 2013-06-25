dfdbc8c2095398624e2dbbbc5aa2d9171384c544
unknown <kanchanm@KANCHANMPOWER.redmond.corp.microsoft.com>
2013-06-24 18:23:25 -0700

    Adding sample for WebAPI/OWIN integration

---
+++ Samples/WebApi/WebAPIOWIN/WebAPIOWIN/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.Owin.Hosting;
+
+namespace WebAPIOWIN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseAddress = string.Format("http://localhost:1025");
+
+            //Start OWIN host
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                //Create HttpCient and make a request to api/values
+                HttpClient client = new HttpClient();
+
+                var response = client.GetAsync(baseAddress + "/api/values").Result;
+
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
