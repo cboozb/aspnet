9623a2635d597c38d777285159e795834f4afa54
davidmatson <dmatson@microsoft.com>
2013-11-26 14:48:03 -0800

    Add Web API action results sample.

---
+++ Samples/WebApi/ActionResults/ActionResults/Controllers/ApiControllerExtensions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Web;
+using System.Web.Http;
+using ActionResults.Results;
+
+namespace ActionResults.Controllers
+{
+    static class ApiControllerExtensions
+    {
+        public static OkHtmlResult Html(this ApiController controller, string content)
+        {
+            if (controller == null)
+            {
+                throw new ArgumentNullException("controller");
+            }
+
+            return new OkHtmlResult(content, Encoding.UTF8, controller.Request);
+        }
+
+        public static OkTextPlainResult Text(this ApiController controller, string content)
+        {
+            return Text(controller, content, Encoding.UTF8);
+        }
+
+        public static OkTextPlainResult Text(this ApiController controller, string content, Encoding encoding)
+        {
+            if (controller == null)
+            {
+                throw new ArgumentNullException("controller");
+            }
+
+            return new OkTextPlainResult(content, encoding, controller.Request);
+        }
+
+        public static OkFileDownloadResult Download(this ApiController controller, string path, string contentType)
+        {
+            string downloadFileName = Path.GetFileName(path);
+            return Download(controller, path, contentType, downloadFileName);
+        }
+
+        public static OkFileDownloadResult Download(this ApiController controller, string path, string contentType, string downloadFileName)
+        {
+            if (controller == null)
+            {
+                throw new ArgumentNullException("controller");
+            }
+
+            return new OkFileDownloadResult(MapPath(path), contentType, downloadFileName, controller.Request);
+        }
+
+        private static string MapPath(string path)
+        {
+            // The following code is for demonstration purposes only and is not fully robust for production usage.
+            // HttpContext.Current is not always available after asynchronous calls complete.
+            // Also, this call is host-specific and will need to be modified for other hosts such as OWIN.
+            return HttpContext.Current.Server.MapPath(path);
+        }
+    }
+}
---
+++ Samples/WebApi/ActionResults/ActionResults/Results/OkFileDownloadResult.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace ActionResults.Results
+{
+    public class OkFileDownloadResult : IHttpActionResult
+    {
+        public OkFileDownloadResult(string localPath, string contentType, string downloadFileName,
+            HttpRequestMessage request)
+        {
+            if (localPath == null)
+            {
+                throw new ArgumentNullException("localPath");
+            }
+
+            if (contentType == null)
+            {
+                throw new ArgumentNullException("contentType");
+            }
+
+            if (downloadFileName == null)
+            {
+                throw new ArgumentNullException("downloadFileName");
+            }
+
+            if (request == null)
+            {
+                throw new ArgumentNullException("request");
+            }
+
+            LocalPath = localPath;
+            ContentType = contentType;
+            DownloadFileName = downloadFileName;
+            Request = request;
+        }
+
+        public string LocalPath { get; private set; }
+
+        public string ContentType { get; private set; }
+
+        public string DownloadFileName { get; private set; }
+
+        public HttpRequestMessage Request { get; private set; }
+
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            return Task.FromResult(Execute());
+        }
+
+        private HttpResponseMessage Execute()
+        {
+            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Content = new StreamContent(File.OpenRead(LocalPath));
+            response.Content.Headers.ContentType = MediaTypeHeaderValue.Parse(ContentType);
+            response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
+            {
+                FileName = DownloadFileName
+            };
+            return response;
+        }
+    }
+}
