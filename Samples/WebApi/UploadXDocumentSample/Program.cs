949d09020e09c1c3af3d36fa6612b443a00f72f4
kiran <kichalla@microsoft.com>
2014-01-15 15:13:38 -0800

    Updated UploadXDocumentSample to use latest packages and also converted to OWIN Selfhost

--- Samples/WebApi/UploadXDocumentSample/Program.cs
+++ Samples/WebApi/UploadXDocumentSample/Program.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.IO;
 using System.Net.Http;
-using System.ServiceModel;
 using System.Web.Http;
-using System.Web.Http.SelfHost;
 using System.Xml.Linq;
+using Microsoft.Owin.Hosting;
+using Owin;
 
 namespace UploadXDocumentSample
 {
@@ -13,28 +13,16 @@ namespace UploadXDocumentSample
     /// </summary>
     class Program
     {
-        static readonly Uri _baseAddress = new Uri("http://localhost:50231");
+        static readonly string _baseAddress = "http://localhost:50231";
 
         static void Main(string[] args)
         {
-            HttpSelfHostServer server = null;
+            IDisposable server = null;
+
             try
             {
-                // Set up server configuration
-                HttpSelfHostConfiguration config = new HttpSelfHostConfiguration(_baseAddress);
-                config.HostNameComparisonMode = HostNameComparisonMode.Exact;
+                server = WebApp.Start<Program>(url: _baseAddress);
 
-                config.Routes.MapHttpRoute(
-                    name: "DefaultApi",
-                    routeTemplate: "api/{controller}/{id}",
-                    defaults: new { id = RouteParameter.Optional }
-                );
-
-                // Create server
-                server = new HttpSelfHostServer(config);
-
-                // Start listening
-                server.OpenAsync().Wait();
                 Console.WriteLine("Listening on " + _baseAddress);
 
                 // Run HttpClient issuing requests
@@ -55,9 +43,22 @@ namespace UploadXDocumentSample
                 if (server != null)
                 {
                     // Stop listening
-                    server.CloseAsync().Wait();
+                    server.Dispose();
                 }
             }
+        }
+
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var config = new HttpConfiguration();
+
+            config.Routes.MapHttpRoute(
+                    name: "DefaultApi",
+                    routeTemplate: "api/{controller}/{id}",
+                    defaults: new { id = RouteParameter.Optional }
+                );
+        
+            appBuilder.UseWebApi(config);
         }
 
         static async void RunClient()
@@ -65,19 +66,25 @@ namespace UploadXDocumentSample
             // Create an HttpClient instance
             HttpClient client = new HttpClient();
 
+            // Use chunked encoding as otherwise HttpClient would try buffering the content to 
+            // figure out the content length.
+            client.DefaultRequestHeaders.TransferEncodingChunked = true;
+
             // Create a push content so that we can use XDocument.Save to a stream
             XDocument xDoc = XDocument.Load("Sample.xml", LoadOptions.None);
             PushStreamContent xDocContent = new PushStreamContent(
                 (stream, content, context) =>
                 {
                     // After save we close the stream to signal that we are done writing.
-                    xDoc.Save(stream);
-                    stream.Close();
+                    using (stream)
+                    {
+                        xDoc.Save(stream);
+                    }
                 },
                 "application/xml");
 
             // Send POST request to server and wait asynchronously for the response
-            Uri address = new Uri(_baseAddress, "/api/book");
+            Uri address = new Uri(_baseAddress + "/api/book");
             HttpResponseMessage response = await client.PostAsync(address, xDocContent);
 
             // Ensure we get a successful response.
