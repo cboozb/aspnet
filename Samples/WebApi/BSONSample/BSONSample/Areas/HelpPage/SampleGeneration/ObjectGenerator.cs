5b4591fb3ee308e418f5d5274fec9b2528220757
Ian Hong <ianhong@outlook.com>
2014-01-15 14:59:22 -0800

    Updating BsonFormatterSample for WSR 5.1

--- Samples/WebApi/BSONSample/BSONSample/Areas/HelpPage/SampleGeneration/ObjectGenerator.cs
+++ Samples/WebApi/BSONSample/BSONSample/Areas/HelpPage/SampleGeneration/ObjectGenerator.cs
@@ -13,7 +13,7 @@ namespace BSONSample.Areas.HelpPage
     /// </summary>
     public class ObjectGenerator
     {
-        private const int DefaultCollectionSize = 2;
+        internal const int DefaultCollectionSize = 2;
         private readonly SimpleTypeObjectGenerator SimpleObjectGenerator = new SimpleTypeObjectGenerator();
 
         /// <summary>
