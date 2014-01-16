cb1ce451f310ec94974e2ce3c97b5fc78ff90bd8
kiran <kichalla@microsoft.com>
2014-01-15 18:28:59 -0800

    Updated AzureBlobsFileUploadSample to use latest packages

--- Samples/WebApi/AzureBlobsFileUploadSample/AzureBlobsFileUpload/Global.asax.cs
+++ Samples/WebApi/AzureBlobsFileUploadSample/AzureBlobsFileUpload/Global.asax.cs
@@ -18,7 +18,7 @@ namespace AzureBlobsFileUpload
         {
             AreaRegistration.RegisterAllAreas();
 
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
