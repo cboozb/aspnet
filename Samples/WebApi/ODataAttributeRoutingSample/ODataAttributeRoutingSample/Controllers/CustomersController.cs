b484b5e10d192a628f5df5b2015cad60c3eff897
fenzhao <fenzhao@microsoft.com>
2014-02-14 15:23:51 +0800

    Add WebApi OData Attribute Routing Sample

---
+++ Samples/WebApi/ODataAttributeRoutingSample/ODataAttributeRoutingSample/Controllers/CustomersController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.OData;
+using System.Web.OData.Routing;
+using ODataAttributeRoutingSample.Models;
+
+namespace ODataAttributeRoutingSample.Controllers
+{
+    public class CustomersController : ODataController
+    {
+        private static IEnumerable<Customer> Customers = Enumerable.Range(0, 4).Select(i =>
+            new Customer
+            {
+                Id = i,
+                Name = "Customer " + i,
+                Orders = Enumerable.Range(0, i).Select(j =>
+                    new Order
+                    {
+                        Id = i * 10 + j,
+                        Price = (j + 1) * 1.11
+                    }
+                )
+            });
+
+        public IEnumerable<Customer> Get()
+        {
+            return Customers;
+        }
+
+        [ODataRoute("GetCustomersWithNameContaining(ContainedString={str})")]
+        public IEnumerable<Customer> GetSpecialCustomers([FromODataUri]string str)
+        {
+            return Customers.Where(customer => customer.Name.Contains(str));
+        }
+
+        [ODataRoute("Customers({customerId})/Orders({orderId})/Default.UpdateOrderPrice")]
+        public Order UpdateOrderPrice(int customerId, int orderId, ODataActionParameters param)
+        {
+            Order order = Customers.Single(c => c.Id == customerId).Orders.Single(o => o.Id == orderId);
+            order.Price = Double.Parse(param["Price"].ToString());
+            return order;
+        }
+
+        [HttpGet]
+        [ODataRoute("Customers({customerId})/Default.DeleteOrderFromCustomer(OrderId={id})")]
+        public IEnumerable<Order> DeleteOrder(int customerId, int id)
+        {
+            Customer customer = Customers.Single(c => c.Id == customerId);
+            customer.Orders = customer.Orders.Where(o => o.Id != id);
+            return customer.Orders;
+        }
+
+        [ODataRoute("Customers")]
+        public IEnumerable<Customer> WhateverName()
+        {
+            return Customers;
+        }
+    }
+}
---
+++ Samples/WebApi/ODataAttributeRoutingSample/ODataAttributeRoutingSample/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace ODataAttributeRoutingSample.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public IEnumerable<Order> Orders { get; set; }
+    }
+}
