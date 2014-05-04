c2fbb54ccbbd0bb2a50932696d8335a833f8f034
jinfutan <jinfutan@microsoft.com>
2014-05-04 00:16:19 -0700

    Add Singleton Sample

---
+++ Samples/WebApi/OData/v4/ODataSingletonSample/ODataSingletonSample/App_Start/WebApiConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http;
+using System.Web.OData.Extensions;
+
+namespace WebStack.QA.Test.OData.Singleton
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.Always;
+
+            config.Routes.MapODataServiceRoute("odata", "odata", SingletonEdmModel.GetEdmModel()).MapODataRouteAttributes(config);
+            config.EnsureInitialized();
+        }
+    }
+}
