dfc0abf00042ed058e03c1dafaa221acbb1ff8ee
jacalvar <jacalvar@microsoft.com>
2014-06-17 16:32:06 -0700

    Added a sample that demonstrates how to do transactions in $batch requests with Entity Framework and datajs.

---
+++ Samples/WebApi/OData/v3/ODataEFBatchSample/ODataEFBatchSample/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace ODataEFBatchSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return Redirect("/Tests/SpecRunner.html");
+        }
+    }
+}
