d1f41969686424f174eee496f42fa1df58335933
jacalvar <jacalvar@microsoft.com>
2014-06-30 13:51:41 -0700

    Updated WebForms sample packages to 5.2.0 RTM.

--- Samples/WebApi/WebFormSample/Client/Program.cs
+++ Samples/WebApi/WebFormSample/Client/Program.cs
@@ -1,45 +1,33 @@
 ﻿using System;
 using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Client
 {
     /// <summary>
-    /// This sample client tries out the ContentController exposed by the ContentController project in this solution.
-    /// It first issues a PUT request and then a GET request.
+    /// This sample client tries out the ValuesController exposed by the WebForm project in this solution.
+    /// It issues a GET request to retrieve the list of values and displays the results on screen.
     /// </summary>
     class Program
     {
-        static readonly Uri _addres = new Uri("http://localhost:65291/api/values");
+        static readonly Uri _address = new Uri("http://localhost:65291/api/values");
 
-        static void RunClient()
+        static async Task RunClient()
         {
             HttpClient client = new HttpClient();
 
             // Issue GET request against Web API
-            client.GetAsync(_addres).ContinueWith(
-                (getTask) =>
-                {
-                    if (getTask.IsCanceled)
-                    {
-                        return;
-                    }
-                    if (getTask.IsFaulted)
-                    {
-                        throw getTask.Exception;
-                    }
-                    HttpResponseMessage getResponse = getTask.Result;
+            HttpResponseMessage getResponse = await client.GetAsync(_address);
+            getResponse.EnsureSuccessStatusCode();
 
-                    getResponse.EnsureSuccessStatusCode();
-
-                    string result = getResponse.Content.ReadAsStringAsync().Result;
-                    Console.WriteLine("Received response: {0}", result);
-                });
+            string result = getResponse.Content.ReadAsStringAsync().Result;
+            Console.WriteLine("Received response: {0}", result);
         }
 
         static void Main(string[] args)
         {
-            RunClient();
+            RunClient().Wait();
 
             Console.WriteLine("Hit ENTER to exit...");
             Console.ReadLine();
