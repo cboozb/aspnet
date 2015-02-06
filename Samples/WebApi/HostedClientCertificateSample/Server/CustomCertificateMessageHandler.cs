c25cf759382d15e95bbb0ad919c49f9eb0675d27
Doug Bunting <dougbu@microsoft.com>
2015-02-06 11:08:18 -0800

    Update HostedClientCertificateSample to v5.2.3 - correct service project to deploy to full IIS; does not work elsewhere
    nits:
    - clarify some of the explanatory comments
    - remove trailing whitespace from updated files

--- Samples/WebApi/HostedClientCertificateSample/Server/CustomCertificateMessageHandler.cs
+++ Samples/WebApi/HostedClientCertificateSample/Server/CustomCertificateMessageHandler.cs
@@ -8,18 +8,18 @@ using System.Web;
 namespace HostedClientCertificateSample
 {
     /// <summary>
-    /// This verifies the client certificate is one of those known certificates and creates an generic principals with "Administrators" role. 
+    /// This verifies the client certificate is one of those known certificates and creates a generic principal with "Administrators" role.
     /// </summary>
     public class CustomCertificateMessageHandler : DelegatingHandler
     {
-        public const string ClientCertHash = "496B1B29D45375D49270966A45811E9F0AC870E2";
+        public const string ClientCertHash = "97526DCB0CC8D557436DBA57EE1116CFB81E80E1";
 
         protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
             // Retrieving the Client certificate sent by the client
             X509Certificate cert = request.GetClientCertificate();
 
-            // The following code SHOULD be replaced with some custom mapping logic from client certificate to their roles. 
+            // The following code SHOULD be replaced with some custom mapping logic from client certificate to their roles.
             if (cert != null)
             {
                 if (cert.GetCertHashString() == ClientCertHash)
